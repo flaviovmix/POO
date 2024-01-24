ea27ed18a847b5c0bda7ef2cf5c6835246424df4
Flavio vMix <flaviovmix@gmail.com>
2024-01-24 15:14:05 -0300

    PREPARANDO PARA CURSO 03 - LISTAS E COLEÇÕES DE DADOS

---
+++ 03 - LISTAS E COLEÇÕES DE DADOS/src/ItensDaTela.java
@@ -0,0 +1,14 @@
+public class ItensDaTela {
+     
+    public void todaTela(){
+        System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+    }
+
+    public void pequenoEspaco(){
+        System.out.println("\n");
+    }
+
+    public void linhas() {
+        System.out.println("----------------------------");
+    }
+}
